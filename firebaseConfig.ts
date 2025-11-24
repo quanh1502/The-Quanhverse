b7f0afa8fb23bd7d5ebd22884b35d10a90630720
Ten Cua Ban <email_cua_ban@gmail.com>
2025-11-24 18:52:35 +0700

    Sua ten project

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -5,9 +5,9 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   
   apiKey: "AIzaSyCWaE1stn3VJ1Z8OCh2lr_8TIR4hOxJ2oQ", 
-  authDomain: "the-quanhverse.firebaseapp.com",
-  projectId: "the-quanhverse",
-  storageBucket: "the-quanhverse.firebasestorage.app",
+  authDomain: "thequanhverse.firebaseapp.com",
+  projectId: "thequanhverse",
+  storageBucket: "thequanhverse.firebasestorage.app",
   messagingSenderId: "821696410018",
   appId: "1:821696410018:web:c281d20a3c07be6da542fb",
   measurementId: "G-3D8PVE6C1G"
